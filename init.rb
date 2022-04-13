7755c555ef5206640a69ffe1adb4e034f0dbf931
bender <b.lienert@ablida.de>
2022-04-13 15:47:21 +0200

    ensure compatibility with redmine 5.0

--- init.rb
+++ init.rb
@@ -1,16 +1,18 @@
-require 'locale_integer_format'
+# frozen_string_literal: true
+
+require File.expand_path('../lib/locale_integer_format.rb', __FILE__)
 
 Redmine::Plugin.register :locale_integer_format do
   name 'Locale Integer Format plugin'
   author 'benl96'
   description 'Enables input and display of integer custom fields with decimal separator set in locale.'
-  version '0.0.2'
+  version '0.0.3'
   url 'https://github.com/benl96/locale-integer-format'
   author_url 'https://github.com/benl96/locale-integer-format'
 
   RedmineApp::Application.config.after_initialize do
     #Issue.send(:include, LocaleIntegerFormat::IssuePatch)
     #ApplicationHelper.send(:include, LocaleIntegerFormat::ApplicationHelperPatch)
-    Redmine::FieldFormat::IntFormat.send(:include, LocaleIntegerFormat::IntegerFormatPatch)
+    Redmine::FieldFormat::IntFormat.include LocaleIntegerFormat::IntegerFormatPatch
   end
 end
